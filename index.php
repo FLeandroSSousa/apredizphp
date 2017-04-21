5bf17206caccfba86db3106b743b2bbe4c13399c
francisco <fleandro.santos@gmail.com>
2017-04-21 19:46:27 -0300

    inde

---
+++ index.php
@@ -0,0 +1,30 @@
+
+       <?php include ('include/top.php'); ?>
+
+
+	<!---  -->
+	<div class="row">
+		<div class="medium-4 large-4 columns blue">Logo</div>
+		<div class="medium-8 large-8 columns red">menu</div>
+	</div>
+	<div class="row">
+		<div class="medium-2 large-2 columns blue" ></div>
+		<div class="medium-10 large-10 columns red" >ola</div>
+	</div>
+	<div class="row">
+		<div class="medium-12 large-12 blue">Rodape </div>
+	</div>
+                
+                <form action="?controle=Controler&amp;acao=novo" method="post">
+                    nome<input type="text" id="nome_prod" size="45"></div>
+        Cpf<input type="text" id="cpf" size="10"></div>
+        senha<input type="text" id="senha" size="10"></div>
+        <input  id="senha" class="button" type="submit" value="Salvar"></div>
+                </form>
+                
+	<script src="js/vendor/jquery.js"></script>
+    <script src="js/vendor/what-input.js"></script>
+    <script src="js/vendor/foundation.js"></script>
+    <script src="js/app.js"></script>
+    </body>
+</html>
